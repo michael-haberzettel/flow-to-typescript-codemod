13d79f253489d18542b17e65bf983a0119b06751
Michael HABERZETTEL <michael.haberzettel@mention.com>
2023-05-05 06:46:02 +0200

    update global mention files to copy

--- mention-files-to-copy/global.d.ts
+++ mention-files-to-copy/global.d.ts
@@ -1,47 +1,56 @@
-import { App } from '../flow-libs/declarations/App'
-import undescore from 'underscore'
-import moment from 'moment'
-import normalizr from 'normalizr'
+import { App } from "../flow-libs/declarations/App";
+import undescore from "underscore";
+import moment from "moment";
+import normalizr from "normalizr";
+import JQuery from "jquery";
+import d3 from "@types/d3";
 
 declare global {
-    interface JQueryStatic {
-        client?: {
-            browser: string
-            os: string
-        }
+  interface Window {
+    $: JQueryStatic;
+    Backbone: any;
+  }
 
-        SafeDeferred: any
-        (...args: any[]): void
-    }
+  interface JQueryStatic {
+    client?: {
+      browser: string;
+      os: string;
+    };
 
-    interface JQuery {
-        highlight(...args: any[]): void
-        highlightBoolean(...args: any[]): void
-    }
+    SafeDeferred: any;
+    (...args: any[]): void;
+  }
 
-    var moment: moment
-    var Normalizr: normalizr
-    var Intercom: any
-    var toastr: any
-    var __webpack_public_path__: string
-    var App: App
-    var _: typeof undescore
-    var analytics: any
-    var Translation: any
-    var trans: any
-    var transDesc: any
-    var ___trans_desc___: any
-    interface Window {
-        App: App
-        _: typeof undescore
-        analytics: any
-        Translator: any
-        Translation: any
-        trans: any
-        transDesc: any
-        ___trans_desc___: any
-    }
-    type Environment = any
-    type FragmentReference = any
+  interface JQuery {
+    highlight(...args: any[]): void;
+    highlightBoolean(...args: any[]): void;
+  }
+
+  var d3: d3;
+  var dispatcher: { dispatchLifecycle: (ns: string) => void };
+  var moment: moment;
+  var Normalizr: normalizr;
+  var Intercom: any;
+  var toastr: any;
+  var __webpack_public_path__: string;
+  var App: App;
+  var _: typeof undescore;
+  var analytics: any;
+  var Translation: any;
+  var trans: any;
+  var transDesc: any;
+  var ___trans_desc___: any;
+  interface Window {
+    App: App;
+    _: typeof undescore;
+    analytics: any;
+    Translator: any;
+    Translation: any;
+    trans: any;
+    transDesc: any;
+    ___trans_desc___: any;
+  }
+  type Environment = any;
+  type FragmentReference = any;
 }
-export {}
+export {};
--- mention-files-to-copy/modules.d.ts
+++ mention-files-to-copy/modules.d.ts
@@ -1,14 +1,46 @@
-declare module 'mention/legacy/*' {
-    var x: any
-    export = x
-}
+declare module "@chargebee/chargebee-js-react-wrapper" {
+  export type ChargeBeeErrorsStateEntry = {
+    errorCode: string;
+    message: string;
+  };
+  export type ChargeBeeStatusChanged = {
+    empty: boolean;
+    field: string;
+    error?: ChargeBeeErrorsStateEntry;
+  };
+  export type Props = {
+    currency?: string;
+    onBlur?: () => void;
+    onFocus?: () => void;
+    onChange?: (status: ChargeBeeStatusChanged) => void;
+    locale?: string;
+    styles?: any;
+    ref?: any;
+  };
 
-declare module 'mention/integrations/*' {
-    var x: any
-    export = x
-}
+  export type CardComponentApiRef = {
+    authorizeWith3ds(payementIntent: any, additionalInfos: any): Promise<any>;
+    tokenize(additionalInfos: any): Promise<any>;
+    focus(): void;
+    clear(): void;
+  };
 
-declare module 'mention/vendor/*' {
-    var x: any
-    export = x
+  export class CardComponent extends React.Component<Props> {}
 }
+declare module "@lexical/*";
+declare module "browser-filesaver";
+declare module "copy-to-clipboard";
+declare module "draft-js-emoji-mart-plugin";
+declare module "draft-js-mention-plugin";
+declare module "draft-js-plugins-editor";
+declare module "emoji-mart";
+declare module "find-with-regex";
+declare module "mention/integrations/*";
+declare module "mention/legacy/*";
+declare module "mention/vendor/*";
+declare module "moment-timezone";
+declare module "react-date-picker";
+declare module "react-select2-wrapper";
+declare module "react-visibility-sensor";
+declare module "userflow.js";
+declare module "xlsx";
